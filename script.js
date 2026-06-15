ba5cc080765be55bf112b0323792618f7face10e
“hello-id” <helloteam.id@gmail.com>
2026-06-15 10:52:00 +0530

    feat: add play/pause functionality in song items

--- script.js
+++ script.js
@@ -106,15 +106,25 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) =>
     element.addEventListener('click', (e) => {
         makeAllPlays();
         songIndex = parseInt(e.target.id)
-        e.target.classList.remove('fa-circle-play');
-        e.target.classList.add('fa-circle-pause');
-        audioElement.src = `songs/${songIndex + 1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
+
+        if (audioElement.paused || audioElement.currentTime <= 0) {
+            e.target.classList.remove('fa-circle-play');
+            e.target.classList.add('fa-circle-pause');
+            audioElement.src = `songs/${songIndex + 1}.mp3`;
+            masterSongName.innerText = songs[songIndex].songName;
+            audioElement.currentTime = 0;
+            audioElement.play();
+            gif.style.opacity = 1;
+            masterPlay.classList.remove('fa-circle-play');
+            masterPlay.classList.add('fa-circle-pause');
+        } else {
+            audioElement.pause();
+            gif.style.opacity = 0;
+            e.target.classList.remove('fa-circle-pause');
+            e.target.classList.add('fa-circle-play');
+            masterPlay.classList.remove('fa-circle-pause');
+            masterPlay.classList.add('fa-circle-play');
+        }
     })
 })
 
